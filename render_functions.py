2e7cccde8e78a3fa68e28bc718dd1164eac08643
annaglazyrina <47604256+annaglazyrina@users.noreply.github.com>
2020-04-29 18:58:08 +0300

    Game map, entity and interface rendering

---
+++ render_functions.py
@@ -0,0 +1,62 @@
+import libtcodpy as libtcod
+
+from enum import Enum
+
+class RenderOrder(Enum):
+    CORPSE = 1
+    ITEM = 2
+    ACTOR = 3
+
+def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_color):
+    bar_width = int(float(value) / maximum * total_width)
+
+    libtcod.console_set_default_background(panel, back_color)
+    libtcod.console_rect(panel, x, y, total_width, 1, False, libtcod.BKGND_SCREEN)
+
+    libtcod.console_set_default_background(panel, bar_color)
+    if bar_width > 0:
+        libtcod.console_rect(panel, x, y, bar_width, 1, False, libtcod.BKGND_SCREEN)
+
+    libtcod.console_set_default_foreground(panel, libtcod.white)
+    libtcod.console_print_ex(panel, int(x + total_width / 2), y, libtcod.BKGND_NONE, libtcod.CENTER,
+                             '{0}: {1}/{2}'.format(name, value, maximum))
+
+def render_all(con, panel, entities, player, game_map, message_log, screen_width, screen_height, bar_width, panel_height, panel_y):
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].blocked
+            if wall:
+                libtcod.console_put_char_ex(con, x, y, '"', libtcod.white, libtcod.black)
+            else:
+                libtcod.console_put_char_ex(con, x, y, '_', libtcod.gray, libtcod.black)
+
+    entities_in_render_order = sorted(entities, key=lambda x: x.render_order.value)
+
+    for entity in entities_in_render_order:
+        draw_entity(con, entity, game_map)
+
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+    libtcod.console_set_default_background(panel, libtcod.black)
+    libtcod.console_clear(panel)
+
+    y = 1
+    for message in message_log.messages:
+        libtcod.console_set_default_foreground(panel, message.color)
+        libtcod.console_print_ex(panel, message_log.x, y, libtcod.BKGND_NONE, libtcod.LEFT, message.text)
+        y += 1
+
+    render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp, player.fighter.max_hp,
+               libtcod.light_red, libtcod.darker_red)
+
+    libtcod.console_blit(panel, 0, 0, screen_width, panel_height, 0, 0, panel_y)
+
+def clear_all(con, entities):
+    for entity in entities:
+        clear_entity(con, entity)
+
+def draw_entity(con, entity, game_map):
+    libtcod.console_set_default_foreground(con, entity.color)
+    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+
+def clear_entity(con, entity):
+    libtcod.console_put_char_ex(con, entity.x, entity.y, '_', libtcod.gray, libtcod.black)
